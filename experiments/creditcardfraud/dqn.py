b8b656a6a472c13bc7d382d82cded8c591dac19c
denbergvanthijs <denbergvanthijs@gmail.com>
2020-11-23 14:17:31 +0100

    Changed parameters

--- experiments/creditcardfraud/dqn.py
+++ experiments/creditcardfraud/dqn.py
@@ -7,21 +7,23 @@ from imbDRL.metrics import classification_metrics, network_predictions
 from tf_agents.environments.tf_py_environment import TFPyEnvironment
 from tqdm import tqdm
 
-episodes = 50_000  # Total number of episodes
-warmup_episodes = 60_000  # Amount of warmup steps to collect data with random policy
+episodes = 1_000  # Total number of episodes
+warmup_episodes = 170_000  # Amount of warmup steps to collect data with random policy
 memory_length = warmup_episodes  # Max length of the Replay Memory
+batch_size = 2048
+collect_steps_per_episode = 0
 
-target_model_update = episodes // 100  # Period to overwrite the target Q-network with the default Q-network
-target_update_tau = 0.01  # Soften the target model update
+target_model_update = episodes // 50  # Period to overwrite the target Q-network with the default Q-network
+target_update_tau = 0.9  # Soften the target model update
 
 conv_layers = None  # Convolutional layers
 dense_layers = (256, 256, )  # Dense layers
 dropout_layers = (0.2, 0.2, )  # Dropout layers
 
 lr = 0.001  # Learning rate
-gamma = 0.0  # Discount factor
+gamma = 0.95  # Discount factor
 min_epsilon = 0.05  # Minimal and final chance of choosing random action
-decay_episodes = 25_000  # Number of episodes to decay from 1.0 to `min_epsilon`
+decay_episodes = episodes // 10  # Number of episodes to decay from 1.0 to `min_epsilon`
 
 imb_rate = 0.001729  # Imbalance rate
 min_class = [1]  # Labels of the minority classes
@@ -45,8 +47,9 @@ for _ in tqdm(range(10)):
     # Change Python environment to TF environment
     train_env = TFPyEnvironment(ClassifyEnv(X_train, y_train, imb_rate))
 
-    model = TrainCustomDDQN(episodes, warmup_episodes, lr, gamma, min_epsilon, decay_episodes,
-                            target_model_update=target_model_update, target_update_tau=target_update_tau, progressbar=False)
+    model = TrainCustomDDQN(episodes, warmup_episodes, lr, gamma, min_epsilon, decay_episodes, target_model_update=target_model_update,
+                            target_update_tau=target_update_tau, progressbar=False, batch_size=batch_size,
+                            collect_steps_per_episode=collect_steps_per_episode)
 
     model.compile_model(train_env, conv_layers, dense_layers, dropout_layers)
     model.train(X_val, y_val)
