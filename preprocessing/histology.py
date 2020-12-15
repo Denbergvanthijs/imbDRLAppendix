4c216357e2fe084c0dc8ef4fbcffe0bf1c9cd0ff
denbergvanthijs <denbergvanthijs@gmail.com>
2020-12-15 13:34:39 +0100

    Added relabeling of data

--- preprocessing/histology.py
+++ preprocessing/histology.py
@@ -3,16 +3,22 @@ import os
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
 import tensorflow as tf
 
+sns.set_palette("colorblind")
+
 parser = argparse.ArgumentParser(description="Generates tf.dataset based on Path argument.")
-parser.add_argument("filepath", metavar="Path", type=str, nargs="?", default="./data/hist", help="The path to the folder containing PNGs.")
+parser.add_argument("imagepath", metavar="Path", type=str, nargs="?", default="./data/hist", help="The path to the folder containing PNGs.")
+parser.add_argument("csvpath", metavar="Path", type=str, nargs="?", default="./data/AE_20201412.csv", help="The path to the csv-file.")
 
 
 @tf.function
-def fp_to_resized_image(filepath):
+def fp_to_resized_image(filepath: str):
     parts = tf.strings.split(filepath, sep=os.sep)
-    filename = parts[-1]
+    # Only keep filename, remove AE from AE1234
+    filename = tf.strings.substr(tf.strings.split(parts[-1], sep=".", maxsplit=1)[0], pos=2, len=4)
     image = tf.io.read_file(filepath)
     image = tf.image.decode_png(image, channels=4)  # Skip alpha layer
     image = tf.image.convert_image_dtype(image, tf.float32)  # Convert range 0-255 to 0-1
@@ -33,22 +39,70 @@ def generate_dataset(filepath: str):
     X_data, y_data = np.empty(1), np.empty(1)
     for images, labels in ds_images.take(-1):
         X_data = images.numpy()
-        y_data = np.vectorize(lambda x: x.decode("utf-8"))(labels.numpy())
+        y_data = np.vectorize(lambda x: x.decode("utf-8"))(labels.numpy())  # Decode TF bytes to python strings
 
+    y_data = y_data.astype(np.int32)
     return X_data, y_data
+
+
+def read_dataframe(filepath: str):
+    dtypes = {"STUDY_NUMBER": int, "Age": int, "Gender": "category",
+              "Hospital": "category", "dateok": str, "arteryop": int, "restenos": str}
+    df = pd.read_csv(filepath, dtype=dtypes, parse_dates=["dateok"], index_col="STUDY_NUMBER")
+    df.drop(columns=["Unnamed: 0"], inplace=True)  # Drop old index-col from original csv-file
+    df["restenos"].fillna(-1.0, inplace=True)  # Fill NaNs, it is up to the user to determine the fate of missing values
+    df["restenos"] = df["restenos"].astype(int)
+    arteryop_categories = np.arange(df["arteryop"].min(), df["arteryop"].max() + 1)
+    df["arteryop"] = pd.Categorical(df["arteryop"], categories=arteryop_categories, ordered=True)  # For plotting the data ordered
+    return df
+
+
+def relabel_by_column(y_data: np.ndarray, column: pd.Series, default: int = -1):
+    """
+    Relabels `y_data` array by looking for corresponding value in `column` Series.
+    If value at y_data is not available in `column`, a default value will be chosen.
+    Values in `y_data` must correspond to index of `column`.
+    """
+    d = column.to_dict()
+    _y_data = y_data.copy()
+    for c, value in enumerate(_y_data):
+        _y_data[c] = d.get(value, default)
+    return _y_data
 
 
 def show(image, label):
     plt.figure()
-    plt.imshow(image, vmin=0, vmax=1)
+    plt.imshow(image, vmin=0, vmax=1, cmap="Greys")
     plt.colorbar()
     plt.title(label)
+    plt.grid(False)
+    plt.show()
+
+
+def data_exploration(df):
+    print(df.head())
+    print(df.describe(include="all"))
+    print(f"Age: {df.Age.mean():.2f}, ± {df.Age.std():.2f}\n"
+          f"Gender:\n{df.Gender.value_counts().to_string()}\n"
+          f"Restenos:\n{df.restenos.value_counts().to_string()}\n"
+          f"Hospital:\n{df.Hospital.value_counts().to_string()}")
+
+    # Distributions
+    _, axis = plt.subplots(1, 2)
+    for c, col in enumerate(["arteryop", "dateok"]):
+        sns.histplot(data=df, x=col, ax=axis[c])
     plt.show()
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    X_data, y_data = generate_dataset(args.filepath + "**/*.png")
+    X_data, y_data = generate_dataset(args.imagepath + "**/*.png")
 
     print(X_data.shape, y_data.shape)
-    # show(X_data[0], y_data[0])
+    print(X_data.dtype, y_data.dtype)
+
+    df = read_dataframe(args.csvpath)
+    # data_exploration(df)
+    y_data_labeled = relabel_by_column(y_data, df["restenos"], default=-1)
+    print(f"Counter: {dict(zip(*np.unique(y_data_labeled, return_counts=True)))}")
+    # show(X_data[0], f"Study Number: {y_data[0]}; Restenosis: {y_data_labeled[0]}")
